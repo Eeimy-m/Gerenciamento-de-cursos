f83ccd5df43c02d038abfa01870658a468e9991a
Elisa <eeimymiura@gmail.com>
2025-10-10 16:06:23 -0300

    adicionar aluno e conferir se cpf já existe

--- mainProgram.c
+++ mainProgram.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct aluno {
-    char cpf[14], nome[40], dataNascimento[10], sexo, emails[10][40], telefones[10][14];
+    char nome[40], dataNascimento[10], sexo, emails[10][40], telefones[10][14]; //CPF é único por aluno 
     //int quantEmails, quantTelefones;
+    int cpf;
 };
 
 struct curso {
-    char codigo[10], descricao[60];
+    char codigo[10], descricao[60]; //código é único por currso
     float preco, cargaHoraria;
 };
 
@@ -48,8 +50,40 @@ void submenuRelatorios(int *opcao) {
     scanf("%d", opcao);
 }
 
+int incluirAluno(struct aluno listaAlunos[], int quant) {
+    int quantEmails = 0, i, quantTelefones = 0;
+    printf("\nNome: ");
+    getchar();
+    fgets(listaAlunos[quant].nome, 40, stdin);
+    printf("\nData de nascimento (dd/mm/aaaa): ");
+    scanf("%s", &listaAlunos[quant].dataNascimento);
+    printf("\nSexo: ");
+    getchar();
+    scanf("%c", &listaAlunos[quant].sexo);
+    printf("\nInforme a quantidade de e-mails a serem inseridos: ");
+    scanf("%d", &quantEmails);
+    printf("\nInsira a seguir o(s) e-mail(s) do aluno: ");
+    for(i = 0; i < quantEmails; i++) {
+        scanf("%s", &listaAlunos[quant].emails[i]);
+    }
+    printf("");
+    printf("\n");
+    printf("\ne-mail(s) inserido(s) com sucesso.");
+    printf("\nInforme a quantidade de números telefônicos: ");
+    scanf("%d", &quantTelefones);
+    printf("\nInsira a seguir o(s) telefone(s): ");
+    for(i = 0; i < quantTelefones; i++) {
+        scanf("%s", &listaAlunos[quant].telefones[i]);
+    }
+    printf("Telefone(s) inserido(s) com sucesso.");
+    printf("\n");
+    return 1;
+}
+
 int main() {
-    int opcao = 0, opcaoSubmenu = 0, opcaoSubmenurelatorios = 0;
+    struct aluno alunos[100];
+    struct curso cursos[20];
+    int opcao = 0, opcaoSubmenu = 0, opcaoSubmenurelatorios = 0, i, quantAlunos = 0, cpfExiste, resultado = 0, cpf;
     do {
         mainMenu(&opcao);
         switch (opcao) {
@@ -65,13 +99,43 @@ int main() {
                         printf("\nVoce selecionou 2");
                         break;
                     case 3:
-                        printf("\nVoce selecionou 3");
+                        cpfExiste = 0;
+                        printf("\nIncluindo aluno no sistema...");
+                        printf("\n");
+                        printf("\n Informe o CPF do aluno (apenas os números): ");
+                        scanf("%d", &cpf);
+                        for(i = 0; i < quantAlunos; i++) {
+                            if(cpf == alunos[i].cpf) {
+                                printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                                printf("\nO CPF inserido já existe no sistema.");
+                                printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                                printf("\n");
+                                cpfExiste = 1;
+                                break;
+                            }
+                        } 
+                        if(cpfExiste == 0 && quantAlunos < 100) {
+                            alunos[quantAlunos].cpf = cpf;
+                            resultado = incluirAluno(alunos, quantAlunos);
+                            if(resultado == 1) { //feedback
+                                quantAlunos++;
+                                printf("\n=============================");
+                                printf("\nAluno adicionado ao sistema com sucesso!");
+                                printf("\n=============================");
+                                printf("\n");
+                            }
+                            else {
+                                printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                                printf("\nNão foi possível adicionar o aluno ao sistema.");
+                                printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                            }
+                        } 
                         break;
                     case 4:
                         printf("\nVoce selecionou 4");
                         break;
-                break;
                 }
+                break;
             case 2:
                 printf("\n=============================");
                 printf("\nSubmenu de Cursos:");
